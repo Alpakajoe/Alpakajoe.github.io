20265afb2fae7b0529cc99ba8d01c1afa62d3b5a
AlpakaJoe <vanessa.traub@web.de>
2021-10-12 08:33:49 +0200

    add images

--- esa1/script.js
+++ esa1/script.js
@@ -1,49 +1,41 @@
 window.onload = () => {
-    let rotationAngle = 0;
-    leng;
-    let characterAnimationStep = 1;
+    let flowerDegree = 0;
+    let rotatingInterval;
     const imageArray = [];
-    const moon = document.getElementById('moon');
-    const character = document.getElementById('character');
+    const flower = document.getElementById('flower');
   
     // to choose the correct image, the angle/step will increase 
-    const getMoonImagePath = (angle) => (`resources/moon-${angle}-degree.png`);
-    const getCharacterImagePath = (step) => (`resources/character-${step}.png`);
+    const getFlowerPath = (angle) => (`resources/flower-${angle}-degree.png`);
 
     // function to load all images to avoid stagnation 
     function loadImages() {
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < 10; i++) {
             let image = new Image();
-            image.src = getMoonImagePath(i*12);
+            image.src = getFlowerPath(i*36);
             imageArray.push(image);
         }
     }
     loadImages();
 
-    setInterval(() => {
-        character.setAttribute('src', getCharacterImagePath(characterAnimationStep));
-        characterAnimationStep === 16 ? characterAnimationStep = 1 : characterAnimationStep++;
-    }, 200);
-
     const stopRotation = () => {
         clearInterval(rotatingInterval);
-    ng = undefined;
+        rotatingInterval = undefined;
     };
 
     const rotateImage = (clockwise) => {
         if (clockwise) {
-            rotationAngle += 12;
+            rotationAngle += 36;
             if (rotationAngle >= 360) {
                 rotationAngle = rotationAngle - 360;
             }
-            moon.setAttribute('src', getMoonImagePath(rotationAngle));
+            flower.setAttribute('src', getFlowerPath(rotationAngle));
             return;
         }
-        rotationAngle -= 12;
+        rotationAngle -= 36;
             if (rotationAngle < 0) {
                 rotationAngle = rotationAngle + 360;
             }
-        moon.setAttribute('src', getMoonImagePath(rotationAngle));
+        flower.setAttribute('src', getFlowerPath(rotationAngle));
     };
 
     const rotateContinuosly = () => {
@@ -53,7 +45,7 @@ window.onload = () => {
     }
 
     window.onkeydown = ((event) => {
-        if (event.key === 'a') {
+        if (event.key === 'f') {
             if (!rotatingInterval) {
                 rotateContinuosly();
                 return;
