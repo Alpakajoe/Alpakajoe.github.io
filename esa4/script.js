706cd19ac481a73ac88f51d818bfdf61dbc8a519
AlpakaJoe <vanessa.traub@web.de>
2021-11-08 15:52:36 +0100

    esa4

---
+++ esa4/script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+
+  let linesBtn = document.getElementById("linesBtn");
+  addEvent(linesBtn, 'click', function() {
+      window.location.href = "index.html";
+  });
+
+  let colorBtn = document.getElementById("colorBtn");
+  addEvent(colorBtn, 'click', function() {
+      window.location.href = "color.html";
+  });
+
+  let ownBtn = document.getElementById("ownBtn");
+  addEvent(ownBtn, 'click', function() {
+      window.location.href = "own.html";
+});
+}
+
+function addEvent(el, eventType, handler) {
+  if (el.addEventListener) { 
+      el.addEventListener(eventType, handler, false);
+  } else if (el.attachEvent) { 
+      el.attachEvent('on' + eventType, handler);
+  } else { 
+      el['on' + eventType] = handler;
+  }
+}
+
+
