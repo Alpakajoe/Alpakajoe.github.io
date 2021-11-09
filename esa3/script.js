119dbb2c2218fa82de9e50406c3c0472f11601fd
AlpakaJoe <vanessa.traub@web.de>
2021-11-09 08:50:37 +0100

    delete width

--- esa3/script.js
+++ esa3/script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
+canvas.width = 600;
+canvas.height = 600;
 const gl = canvas.getContext('experimental-webgl');
     
 // Vertices in array
