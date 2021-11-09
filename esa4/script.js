3db5bfab8e0e7f502ef504cdb1df042f9c039a5f
AlpakaJoe <vanessa.traub@web.de>
2021-11-09 10:34:11 +0100

    window onload

--- esa4/script.js
+++ esa4/script.js
@@ -14,14 +14,14 @@ window.onload = () => {
   addEvent(ownBtn, 'click', function() {
       window.location.href = "own.html";
 });
-}
 
-function addEvent(el, eventType, handler) {
-  if (el.addEventListener) { 
-      el.addEventListener(eventType, handler, false);
-  } else if (el.attachEvent) { 
-      el.attachEvent('on' + eventType, handler);
-  } else { 
-      el['on' + eventType] = handler;
-  }
+    function addEvent(el, eventType, handler) {
+        if (el.addEventListener) { 
+            el.addEventListener(eventType, handler, false);
+        } else if (el.attachEvent) { 
+            el.attachEvent('on' + eventType, handler);
+        } else { 
+            el['on' + eventType] = handler;
+        }
+    }
 }
